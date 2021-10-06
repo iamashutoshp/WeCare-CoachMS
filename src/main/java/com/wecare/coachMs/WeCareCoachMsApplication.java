0c54f42332cc4490b36ff0af9c2f1da831bc6ce9
iamashutoshp <ap1853177@gmail.com>
2021-10-06 20:41:49 +0530

    Coach micro service first commit

---
+++ src/main/java/com/wecare/coachMs/WeCareCoachMsApplication.java
@@ -0,0 +1,13 @@
+package com.wecare.coachMs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeCareCoachMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeCareCoachMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wecare/coachMs/WeCareCoachMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wecare.coachMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeCareCoachMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
